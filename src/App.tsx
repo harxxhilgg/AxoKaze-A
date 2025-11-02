826a70ef8bf979159d3b4180404e048a663728e7
Harshil Patel <harshilp046@gmail.com>
2025-11-02 14:47:45 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import { useQueryState } from "nuqs";
+
+function App() {
+  // regular state
+  // const [count, setCount] = useState(0);
+
+  // NUQS state - stored in URL
+  // const [name, setName] = useQueryState("name");
+  // const [tab, setTab] = useQueryState("tab", { defaultValue: "home" });
+
+  return (
+    <div className="grid place-items-center h-screen">
+      <h1>HEY HEY HEY HEY</h1>
+    </div>
+  );
+}
+
+export default App;
